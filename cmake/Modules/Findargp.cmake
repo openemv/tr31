d0196dbe290e0926bb247d1bc9942cb9dde794a5
Leon Lynch <lynch.leon@gmail.com>
2022-12-05 20:03:03 +0100

    Update CMake module for finding argp
    Also test MacOS builds using brew dependencies

--- cmake/Modules/Findargp.cmake
+++ cmake/Modules/Findargp.cmake
@@ -19,59 +19,72 @@ include(CheckIncludeFile)
 include(CheckSymbolExists)
 include(FindPackageHandleStandardArgs)
 
-# Check whether libc provides argp_parse
-check_function_exists("argp_parse" argp_parse_FOUND)
-
-# Check for argp.h header
+# Check whether system provides argp.h header
 CHECK_INCLUDE_FILE(
 	argp.h
 	argp_header_FOUND
 )
 
-if(argp_header_FOUND)
+if(NOT argp_header_FOUND)
+	# Search for argp.h header elsewhere
 	find_path(argp_INCLUDE_DIR
 		NAMES argp.h
 	)
 
+	if(argp_INCLUDE_DIR)
+		# Ensure that compiler can use argp.h header
+		unset(argp_header_FOUND CACHE)
+		set(CMAKE_REQUIRED_INCLUDES "${argp_INCLUDE_DIR}")
+		CHECK_INCLUDE_FILE(
+			argp.h
+			argp_header_FOUND
+		)
+	endif()
+endif()
+
+# Check whether libc provides argp_parse
+check_function_exists(argp_parse argp_parse_FOUND)
+
+if(NOT arg_parse_FOUND)
+	# Search for argp library
 	find_library(argp_LIBRARY
 		NAMES argp
 	)
 
-	if(argp_LIBRARY AND NOT argp_parse_FOUND)
+	if(argp_LIBRARY)
+		# Ensure that compiler can use argp library
+		unset(argp_parse_FOUND CACHE)
 		set(CMAKE_REQUIRED_LIBRARIES ${argp_LIBRARY})
-		check_symbol_exists("argp_parse" "argp.h" argp_parse_FOUND)
+		check_symbol_exists(argp_parse "argp.h" argp_parse_FOUND)
 	endif()
+endif()
 
-	if(argp_parse_FOUND)
-		if(argp_LIBRARY)
-			set(argp_FOUND_MSG ${argp_LIBRARY})
-		else()
-			set(argp_FOUND_MSG "provided by libc")
-		endif()
+if(argp_header_FOUND AND argp_parse_FOUND)
+	if(argp_LIBRARY)
+		set(argp_FOUND_MSG ${argp_LIBRARY})
+	else()
+		set(argp_FOUND_MSG "provided by libc")
 	endif()
 endif()
 
 find_package_handle_standard_args(argp
 	REQUIRED_VARS
-		argp_FOUND_MSG # NOTE: argp_LIBRARY is not required
+		argp_FOUND_MSG # NOTE: argp_INCLUDE_DIR and argp_LIBRARY are not required
 		argp_parse_FOUND
 		argp_header_FOUND
-		argp_INCLUDE_DIR
 )
 
 if(argp_FOUND)
 	set(argp_INCLUDE_DIRS ${argp_INCLUDE_DIR})
 	set(argp_LIBRARIES ${argp_LIBRARY})
 
-	if(NOT TARGET libargp::argp)
-		if(argp_LIBRARY)
-			add_library(libargp::argp UNKNOWN IMPORTED)
-			set_target_properties(libargp::argp
-				PROPERTIES
-					IMPORTED_LOCATION "${argp_LIBRARY}"
-					INTERFACE_INCLUDE_DIRECTORIES "${argp_INCLUDE_DIR}"
-			)
-		endif()
+	if(argp_LIBRARY AND NOT TARGET libargp::argp)
+		add_library(libargp::argp UNKNOWN IMPORTED)
+		set_target_properties(libargp::argp
+			PROPERTIES
+				IMPORTED_LOCATION "${argp_LIBRARY}"
+				INTERFACE_INCLUDE_DIRECTORIES "${argp_INCLUDE_DIR}"
+		)
 	endif()
 endif()
 
