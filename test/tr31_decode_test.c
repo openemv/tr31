138ef9a69aa265faf2ab0795f7e9904e690d8592
Leon Lynch <lynch.leon@gmail.com>
2023-07-19 20:02:00 +0200

    Update copyright notices

--- test/tr31_decode_test.c
+++ test/tr31_decode_test.c
@@ -1,7 +1,7 @@
 /**
  * @file tr31_decode_test.c
  *
- * Copyright (c) 2020, 2021, 2022 Leon Lynch
+ * Copyright (c) 2020, 2021, 2022, 2023 Leon Lynch
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
